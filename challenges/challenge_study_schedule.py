71ee426c6577aad045fe27260e30116199e97848
Pablo Dominguez <pasaxo@gmail.com>
2023-06-01 16:15:40 -0300

    implementação alogoritmo challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    value = 0
+    if target_time is None:
+        return None
+    for tuple in permanence_period:
+        if not(isinstance(tuple[0], int)) or not(isinstance(tuple[1], int)):
+           return None
+        elif target_time >= tuple[0] and target_time <= tuple[1]:
+            value += 1   
+    return value
