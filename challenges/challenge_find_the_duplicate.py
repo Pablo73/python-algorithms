97b836e69c1f16cb872be995921c594887e6ed00
Pablo Dominguez <pasaxo@gmail.com>
2023-06-02 16:01:53 -0300

    Implementação do algoritmo find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1 or not(isinstance(nums[0], int)) or nums[0] < 0:
+        return False
+    first_number = nums[0]
+
+    for value in nums[1:]:
+        if not(isinstance(value, int)) or value < 0:
+            return False
+        elif first_number == value:
+            return value
+  
+    return find_duplicate(nums[1:])
